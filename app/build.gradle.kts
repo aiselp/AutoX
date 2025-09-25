b68c96c8b6490d4dbc5669104450048fe9dea8d2
heham <hehamster@gmail.com>
2025-09-25 17:58:20 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -251,7 +251,7 @@ dependencies {
     implementation("io.noties.markwon:core:4.6.2")
     implementation(libs.androidx.viewpager2)
     implementation(libs.coil.compose)
-    implementation("com.microsoft.onnxruntime:onnxruntime-android:1.22.0")
+    implementation("com.microsoft.onnxruntime:onnxruntime-android:1.17.3")
 }
 
 fun copyTemplateToAPP(isDebug: Boolean, to: File) {
