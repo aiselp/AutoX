ce3bdd4d6f3bd48c48a5ec5ba4e7e9ede74d5d6e
heham <hehamster@gmail.com>
2025-09-26 20:55:58 +0800

    Update ScriptRuntimeV2.kt

--- autojs/src/main/java/com/stardust/autojs/runtime/ScriptRuntimeV2.kt
+++ autojs/src/main/java/com/stardust/autojs/runtime/ScriptRuntimeV2.kt
@@ -45,37 +45,6 @@ import android.webkit.JavascriptInterface
 import com.stardust.autojs.runtime.ScriptRuntime
 import java.nio.FloatBuffer
 
-class OnnxModule(private val runtime: ScriptRuntime) {
-
-    private val classifiers = mutableMapOf<String, OnnxClassifier>()
-    private val detectors = mutableMapOf<String, OnnxDetector>()
-
-    @JavascriptInterface
-    fun loadClassifier(name: String, path: String) {
-        val c = OnnxClassifier(runtime)
-        c.loadModel(path)
-        classifiers[name] = c
-    }
-
-    @JavascriptInterface
-    fun classify(name: String, input: FloatArray): FloatArray {
-        val c = classifiers[name] ?: throw IllegalArgumentException("Classifier $name not loaded")
-        return c.predict(input)
-    }
-
-    @JavascriptInterface
-    fun loadDetector(name: String, path: String) {
-        val d = OnnxDetector(runtime)
-        d.loadModel(path)
-        detectors[name] = d
-    }
-
-    @JavascriptInterface
-    fun detect(name: String, input: FloatArray): List<OnnxDetector.DetectionResult> {
-        val d = detectors[name] ?: throw IllegalArgumentException("Detector $name not loaded")
-        return d.detect(input)
-    }
-}
 
 class ScriptRuntimeV2(val builder: Builder) : ScriptRuntime(builder) {
     lateinit var consoleExtension: ConsoleExtension
