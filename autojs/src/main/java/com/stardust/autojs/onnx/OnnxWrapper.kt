e6247254797401adca9d1c87da6ab1807e5cd9a9
heham <hehamster@gmail.com>
2025-09-27 19:43:13 +0800

    Update OnnxWrapper.kt

--- autojs/src/main/java/com/stardust/autojs/onnx/OnnxWrapper.kt
+++ autojs/src/main/java/com/stardust/autojs/onnx/OnnxWrapper.kt
@@ -1,4 +1,4 @@
-// autojs/src/main/java/com/stardust/autojs/onnx/OnnxWrapper.kt
+// autojs/src/main/java/com/stardust/autojs/onnx/OnnxWrapper.kt    //fix
 package com.stardust.autojs.onnx
 
 import ai.onnxruntime.OnnxTensor
